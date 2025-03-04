81fdb1d872e3a83b3bd507ecd8c3d7460ec061d8
Ömer Taşcı <65555672+omertaasci@users.noreply.github.com>
2025-03-04 10:55:16 +0000

    Added person class

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 # change it to print your name
 
 print("Hello, I am Omer Tasci 35398")
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+p1 = Person("Ömer", 20)
+print(p1.name)
+print(p1.age)
