8c0fe00e662b975b0f75e26873aedeab3b3aeb4b
Ömer Taşcı <65555672+omertaasci@users.noreply.github.com>
2025-03-06 13:40:05 +0000

    Added adress parameter and updated print

--- main.py
+++ main.py
@@ -3,11 +3,11 @@
 print("Hello, I am Omer Tasci 35398")
 
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age, adress):
         self.name = name
         self.age = age
+        self.adress = adress
 
 
-p1 = Person("Ömer", 20)
-print(p1.name)
-print(p1.age)
+p1 = Person("Ömer", 20, "Warsaw Praga Południe")
+print(f"Name : {p1.name}\nAge : {p1.age}\nAdress : {p1.adress}")
