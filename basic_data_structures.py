4be6df3883663ec9565585885526943023ee065a
Ömer Taşcı <omertaasci12@gmail.com>
2025-03-11 11:11:51 +0100

    Assign data structures to variables

---
+++ basic_data_structures.py
@@ -0,0 +1,11 @@
+my_age = 20
+float_num = 4.5
+my_name = "Ömer Taşcı"
+my_list = [5, "Hello world",93.33]
+my_dict = {
+    "key": "val",
+    "username": "xkraltr"
+}
+my_tup = ("hey",11,19.10)
+my_set = {"a","e"}
+attendance_check = True
